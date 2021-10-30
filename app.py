0992f974c6983dc30cf8dce37a9a640ceca91885
unknown <sbarinathodc@gmail.com>
2021-10-30 13:52:03 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import pickle
+from pandas.core.frame import DataFrame
+import streamlit as st
+import pandas as pd
+import joblib
+import base64
+
+st.set_page_config(page_title='DNA Kingdom prediction')
+st.title('DNA Kingdom prediction 👨‍🔬🧬')
+st.subheader('Using Codon of diverse organisms 🔎 ')
+ 
+upload_file=st.file_uploader('Choose a csv')
+if upload_file is not None :
+    data=pd.read_csv(upload_file)
+    st.write(data)
+    model = pickle.load(open("model.pkl", 'rb'))
+    result=pd.DataFrame(model.predict(data))
+    st.dataframe(result)
+
+download=st.button('Download prediction')
+if download:
+  'Download Started!'
+  csv = result.to_csv(index=False)
+  b64 = base64.b64encode(csv.encode()).decode()  # some strings
+  linko= f'<a href="data:file/csv;base64,{b64}" download="mushrooms_prediction.csv">Download csv file</a>'
+  st.markdown(linko, unsafe_allow_html=True)
+  st.text('--------------------Created By : Sabarinath K --------------------😊')
